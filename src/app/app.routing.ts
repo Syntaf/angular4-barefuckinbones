fe0600f195180ed8fc12d2b1ce7cdffed2e17c54
Syntaf <Gmercer015@gmail.com>
2017-03-16 12:23:11 -0700

    basic angular2 files

---
+++ src/app/app.routing.ts
@@ -0,0 +1,8 @@
+import { RouterModule, Routes } from '@angular/router';
+
+// TODO: implement these routes
+const routes: Routes = [
+  // { path: '/mypath', component: /* new component here */ },
+];
+
+export const routing = RouterModule.forRoot(routes);
