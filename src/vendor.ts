c7ed7f5dbdef633bf7349300b19ea14cd5755924
Syntaf <Gmercer015@gmail.com>
2017-03-16 12:16:33 -0700

    add basic webpack, tsconfig, and main/vendor/polyfills

---
+++ src/vendor.ts
@@ -0,0 +1,13 @@
+// Angular 2
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+
+import 'rxjs';
+import '@angularclass/hmr';
+
+// Add other vendor stuff as needed e.g. Boostrap, etc.. 
+// You can import js, ts, css, sass, ...
